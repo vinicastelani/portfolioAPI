2c4ec5e934ecce1a5da33d0b3635a9cc85c7a0f3
Vinícius Castelani dos Santos <visacastelani@gmail.com>
2020-10-14 20:54:12 -0300

    fix: fixed index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const cors = require("cors");
+dotenv.config();
 
-const app = express()
+const app = express();
 app.use(cors());
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-require('./controller/accessController')(app)
+require("./controller/accessController")(app);
 
-
-app.listen(4000)
+app.listen(process.env.PORT || 4000, () =>
+  console.log(`server listening at port ${process.env.PORT}`)
+);
