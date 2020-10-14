d377f0235ac4ef9aab0f2defd43fa8e454b2040a
Vinícius Castelani dos Santos <visacastelani@gmail.com>
2020-10-14 20:23:22 -0300

    feat: added route to retrieve user

--- src/controller/accessController.js
+++ src/controller/accessController.js
@@ -1,28 +1,34 @@
-const express = require('express');
-const User = require('../models/User')
+const express = require("express");
+const User = require("../models/User");
 const router = express.Router();
 
-router.post('/user', async (req, res) => {
-    try {
-        const user = await User.create(req.body)
-        return res.send({ user })
-    } catch (err) {
-        return res.status(400).send({ error: "erro" })
-    }
-})
+router.post("/user", async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    return res.send({ user });
+  } catch (err) {
+    return res.status(400).send({ error: "erro" });
+  }
+});
 
-router.put('/user/:id', async (req, res) => {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body)
-    if (!user) return res.status(404).send({ error: "Usuário não encontrado" })
-    res.status(200).send({ message: "Sessão atualizada", user: req.body })
-})
+router.put("/user/:id", async (req, res) => {
+  const user = await User.findByIdAndUpdate(req.params.id, req.body);
+  if (!user) return res.status(404).send({ error: "Usuário não encontrado" });
+  res.status(200).send({ message: "Sessão atualizada", user: req.body });
+});
 
-router.get('/teste', (req, res) => {
-    try {
-        res.send("ON")
-    } catch (err) {
-        res.status(400).send({ error: err })
-    }
-})
+router.get("/:id", async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (!user) return res.status(400).send({ error: "Usuário não encontrado" });
+  res.status(200).send({ user });
+});
 
-module.exports = app => app.use('/session', router)
+router.get("/teste", (req, res) => {
+  try {
+    res.send("ON");
+  } catch (err) {
+    res.status(400).send({ error: err });
+  }
+});
+
+module.exports = (app) => app.use("/session", router);
